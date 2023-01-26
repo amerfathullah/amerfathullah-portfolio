9b19018143495f5aa88c2f883dbfb86833d57b45
Amer Fathullah <amerfathullah@rocketmail.com>
2023-01-26 21:46:10 +0800

    interactive nav button

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,13 +5,36 @@ import {AiOutlineUser} from 'react-icons/ai'
 import {BiBook} from 'react-icons/bi'
 // import {RiServiceLine} from 'react-icons/ri'
 import {BiMessageSquareDetail} from 'react-icons/bi'
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 
+const sections = ['about', 'skills', 'contact']
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  
+  const [activeNav, setActiveNav] = useState('')
+  
+  useEffect(() => {
+      const handleScroll = () => {
+          sections.forEach(sectionId => {
+              const root = document.getElementById('root')
+              const rootRect = root.getBoundingClientRect()
+              const section = document.getElementById(sectionId)
+              const sectionRect = section.getBoundingClientRect()
+              console.log(section, sectionRect)
+              if (sectionRect.top <= 1 && sectionRect.bottom >= 0) {
+                setActiveNav('#' + sectionId)
+              }
+              else if (root && rootRect.top === 0) setActiveNav('#root')
+          })
+      }
+      window.addEventListener("scroll", handleScroll)
+      return () => {
+          window.removeEventListener("scroll", handleScroll)
+      }
+  }, [])
+
   return (
     <nav>
-      <a href="#root" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#root" onClick={() => setActiveNav('#root')} className={activeNav === '#root' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#skills" onClick={() => setActiveNav('#skills')} className={activeNav === '#skills' ? 'active' : ''}><BiBook/></a>
       {/* <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><RiServiceLine/></a> */}
