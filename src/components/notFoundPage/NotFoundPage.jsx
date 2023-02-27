93ac49e9ccae75c0cae83e96fb066582cf67f546
Amer Fathullah Nuaim Zakaria <36897396+amerfathullah@users.noreply.github.com>
2023-02-27 09:18:29 +0800

    fix 404 canonical link

--- src/components/notFoundPage/NotFoundPage.jsx
+++ src/components/notFoundPage/NotFoundPage.jsx
@@ -4,10 +4,7 @@ import { Helmet } from 'react-helmet';
 const NotFoundPage = () => {
   return (
     <>
-      <Helmet>
-        <title>404 Page not found</title>
-      </Helmet>
-      <h1>Oops! Page not found</h1>
+      <h1>404 Page not found</h1>
       <p>The page you are looking for does not exist.</p>
     </>
   )
