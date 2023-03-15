d1df2c527b6ce904fabea56100407bd5a3197fa4
Amer Fathullah Nuaim Zakaria <36897396+amerfathullah@users.noreply.github.com>
2023-03-15 17:03:33 +0800

    remove helmet

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,6 @@ import Footer from './components/footer/Footer'
 import NotFoundPage from './components/notFoundPage/NotFoundPage'
 import { Helmet } from 'react-helmet';
 
-const structuredData = {
-  "@context": "https://schema.org",
-  "@type": "Person",
-  "name": "Amer Fathullah Nuaim bin Zakaria",
-  "url": "https://amerfathullah.com",
-  "image": "https://amerfathullah.com/static/media/me-about.51fb11cf969c73f7e273.jpg",
-  "sameAs": [
-    "https://www.linkedin.com/in/amerfathullah",
-    "https://twitter.com/AmerFathullah",
-    "https://www.instagram.com/amer_fathullah",
-    "https://www.facebook.com/amer.fathullah",
-    "https://github.com/amerfathullah"
-  ],
-  "email": "amerfathullah@gmail.com",
-  "gender": "male",
-  "birthDate": "1997-08-17",
-  "jobTitle": "Full-Stack Software Developer",
-  "worksFor": {
-    "@type": "Organization",
-    "name": "TFX Islamic Sdn Bhd"
-  },
-  "nationality": "Malaysian",
-  "description": "Amer Fathullah Nuaim bin Zakaria is a Full-Stack Software Developer based in Malaysia. He have diverse software engineering experience such as front-end development, back-end development, mobile development and machine learning.",
-};
-
 const App = () => {
   const [isValidPage, setIsValidPage] = useState(true);
 
@@ -51,15 +26,6 @@ const App = () => {
 
   return (
     <>
-        <Helmet>
-          <meta name="description" content="Hi, I'm Amer Fathullah. A software developer from Malaysia." />
-          <link rel="canonical" href="https://amerfathullah.com" />
-          <script type="application/ld+json">{JSON.stringify(structuredData)}</script>
-          <meta property="og:title" content="Amer Fathullah" />
-          <meta property="og:description" content="Hi, I'm Amer Fathullah. A software developer from Malaysia." />
-          <meta property="og:image" content="https://amerfathullah.com/static/media/me-about.51fb11cf969c73f7e273.jpg" />
-          <meta property="og:url" content="https://amerfathullah.com/" />
-        </Helmet>
         {isValidPage ? (
           <>
             <Header />
