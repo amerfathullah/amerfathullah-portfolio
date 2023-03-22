90d462e56a18d5ba86d01c002493b8300446e9bf
Amer Fathullah <amerfathullah@rocketmail.com>
2023-03-22 19:05:12 +0800

    add link to main page

--- src/components/notFoundPage/NotFoundPage.jsx
+++ src/components/notFoundPage/NotFoundPage.jsx
@@ -5,6 +5,7 @@ const NotFoundPage = () => {
     <>
       <h1>404 Page not found</h1>
       <p>The page you are looking for does not exist.</p>
+      <a href="/" aria-label="root">Return to Main Page</a>
     </>
   )
 }
