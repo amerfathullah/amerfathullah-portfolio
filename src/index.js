34a6f8762fb82b725c8a2f5570e2a9fc791d8124
Amer Fathullah <amerfathullah@rocketmail.com>
2023-03-23 00:45:50 +0800

    Revert "fix index.js"
    This reverts commit a9c7201d3c7dd868272186c1c4c46a92e30b001e.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
