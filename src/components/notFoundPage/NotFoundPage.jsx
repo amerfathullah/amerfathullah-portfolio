17b25e9a0a5a8d43c579cc6c0ee5acd7a3171fb0
amerfathullah <amer@tfx.global>
2024-05-29 18:50:06 +0800

    create android app

--- src/components/notFoundPage/NotFoundPage.jsx
+++ src/components/notFoundPage/NotFoundPage.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
 const NotFoundPage = () => {
+  useEffect(() => {
+    document.title = '404 Page Not Found';
+  }, []);
+
   return (
     <>
-      <h1>404 Page not found</h1>
+      <h1>404 Page Not Found</h1>
       <p>The page you are looking for does not exist.</p>
       <a href="/" aria-label="root">Return to Main Page</a>
     </>
-  )
+  );
 }
 
-export default NotFoundPage
+export default NotFoundPage;
