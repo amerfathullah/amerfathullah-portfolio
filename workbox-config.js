82371fbe3ad0949ed7fc5fcd89625725b04c449d
amerfathullah <amer@tfx.global>
2024-06-13 16:09:32 +0800

    update workbox config

--- workbox-config.js
+++ workbox-config.js
@@ -1,72 +1,64 @@
+const { ExpirationPlugin } = require('workbox-expiration');
+
 module.exports = {
-    globDirectory: 'build/',
-    globPatterns: [
-      '**/*.{html,js,css,png,jpg,jpeg,svg,gif}'
-    ],
-    swDest: 'build/service-worker.js',
-    clientsClaim: true,
-    skipWaiting: true,
-    runtimeCaching: [
-      {
-        urlPattern: ({ request, url }) => {
-          // Return false to exempt requests from being fulfilled by index.html.
-          // If this isn't a navigation, skip.
-          if (request.mode !== 'navigate') {
-            return false;
-          } 
-          // If this is a URL that starts with /_, skip.
-          if (url.pathname.startsWith('/_')) {
-            return false;
-          } 
-          // If this looks like a URL for a resource, because it contains a file extension, skip.
-          const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
-          if (url.pathname.match(fileExtensionRegexp)) {
-            return false;
-          } 
-          // Return true to signal that we want to use the handler.
-          return true;
-        },
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'app-shell',
-        },
+  globDirectory: 'build/',
+  globPatterns: [
+    '**/*.{html,js,css,png,jpg,jpeg,svg,gif}'
+  ],
+  swDest: 'build/service-worker.js',
+  clientsClaim: true,
+  skipWaiting: true,
+  runtimeCaching: [
+    {
+      urlPattern: ({ request, url }) => {
+        if (request.mode !== 'navigate') {
+          return false;
+        } 
+        if (url.pathname.startsWith('/_')) {
+          return false;
+        }
+        const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
+        if (url.pathname.match(fileExtensionRegexp)) {
+          return false;
+        }
+        return true;
       },
-      {
-        urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
-        handler: 'CacheFirst',
-        options: {
-          cacheName: 'images',
-          plugins: [
-            {
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-              },
-            },
-          ],
-        },
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'app-shell',
       },
-      {
-        urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'static-resources',
-        },
+    },
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg|gif)$/,
+      handler: 'CacheFirst',
+      options: {
+        cacheName: 'images',
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 50,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+          }),
+        ],
       },
-      {
-        urlPattern: ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
-        handler: 'StaleWhileRevalidate',
-        options: {
-          cacheName: 'images',
-          plugins: [
-            {
-              expiration: {
-                maxEntries: 50,
-              },
-            },
-          ],
-        },
+    },
+    {
+      urlPattern: ({ request }) => request.destination === 'script' || request.destination === 'style',
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'static-resources',
       },
-    ],
-  };
-  
+    },
+    {
+      urlPattern: ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
+      handler: 'StaleWhileRevalidate',
+      options: {
+        cacheName: 'images',
+        plugins: [
+          new ExpirationPlugin({
+            maxEntries: 50,
+          }),
+        ],
+      },
+    },
+  ],
+};
